9e6272e6bc0228c1c8a8fa7d30ac4e770a6a6d04
Michael Gorman <michael.gorman@haulhub.com>
2024-01-09 14:33:05 -0600

    board generation and rendering tests

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'debug'
 
 class BoardTest < ActiveSupport::TestCase
   context "associations" do
@@ -34,5 +35,28 @@ class BoardTest < ActiveSupport::TestCase
     assert FactoryBot.build(:board, width: 5, height: 5, mine_count: 24).valid?
     assert_not FactoryBot.build(:board, width: 5, height: 5, mine_count: 25).valid?
     assert_not FactoryBot.build(:board, width: 5, height: 5, mine_count: 26).valid?
+  end
+
+  test 'as_2d_array' do
+    board = FactoryBot.create(:board, width: 5, height: 5)
+    board.tiles.where("y < 2").update_all(is_mine: true)
+    board.tiles.where("y >= 2").update_all(is_mine: false)
+    expected = [
+      Array.new(5, true),
+      Array.new(5, true),
+      Array.new(5, false),
+      Array.new(5, false),
+      Array.new(5, false),
+    ]
+    assert_equal expected, board.as_2d_array
+  end
+
+  test 'generate_board' do
+    board = FactoryBot.build(:board, width: 5, height: 5, mine_count: 12)
+    assert board.tiles.empty?
+    board.save
+    assert_equal 25, board.tiles.count
+    assert_equal 12, board.tiles.where(is_mine: true).count
+    assert_equal 13, board.tiles.where(is_mine: false).count
   end
 end
